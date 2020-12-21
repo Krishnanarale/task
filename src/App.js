4e2a2d310223e5ed3460d05b5c40ada7caedbedc
Krishna Narale <krishnanarale80@gmail.com>
2020-12-21 16:55:23 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <Hello></Hello>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <div className="container">
+      <center><h1>{ 'Hello' }</h1></center>
+    </div>
+  );
+}
+
+export default Hello;
